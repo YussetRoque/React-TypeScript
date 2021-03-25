2bfa9de5a536606b4e8f92549c3bd6e1cf930569
Yusset <Yussetroquecemas@gmail.com>
2021-03-25 02:05:52 -0400

    React + TypeScript: useState

--- mi-app/src/App.tsx
+++ mi-app/src/App.tsx
@@ -1,10 +1,21 @@
 
 
+
+import { Counter } from './Components/Counter';
+import { Usuario } from './Components/Usuario';
+
+
 function App() {
   return (
     <>
     <h1> React + TypeScript </h1>
-    </>
+    <hr/> 
+
+       <Counter/>
+
+      <Usuario />
+   </>
+   
   );
 }
 
